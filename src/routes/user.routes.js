91abad7cc54223828791c256722a1474d43e85a7
bhagyashrithombre <bhagyashrithombre25@gmail.com>
2024-07-04 12:51:25 +0530

    Add comment, like, playlist, tweet models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,6 @@ import {
   refreshAccessToken,
   changeCurrentPassword,
   getCurrentUser,
-  updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
@@ -37,7 +36,6 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
@@ -51,4 +49,5 @@ router
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
+
 export default router;
